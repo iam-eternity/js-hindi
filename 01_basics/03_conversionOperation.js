f6032ebc2e07e884b8cf952d89ac70cc12ae4a75
eternity <114994799+iam-eternity@users.noreply.github.com>
2023-11-24 16:57:38 +0000

    conversion of dataTypes in js

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = undefined 
+console.log(typeof(score))
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Aa"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "Aakash" => true
+
+let someNumber = 69
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
