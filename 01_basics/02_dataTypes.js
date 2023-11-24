35182c17554788c16c1adbd80e077a632649743c
eternity <114994799+iam-eternity@users.noreply.github.com>
2023-11-24 16:31:34 +0000

    datatypes in js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+// alert("hello") // we are using node js, not browser 
+console.log(3 
+    +
+     4) // code readibility should be high
+
+let name = "Aakash"
+let age = 20
+let isLoggedIn = false
+let state;
+// number => 2 to power of 53 (range)
+// bigint 
+// string => ""(preferred)
+// boolean => true/false
+// null => standalone value
+// undefined => value has not been assigned yet
+// symbol => unique
+
+// object
+
+console.log(typeof("Aakash"))
+console.log(typeof(null)) // object
+console.log(typeof(undefined)) // undefined
+
+
+
+
