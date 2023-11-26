36def7e46027e5ed32954c84dc962d42f29cf861
eternity <114994799+iam-eternity@users.noreply.github.com>
2023-11-26 17:40:16 +0000

    strings in js

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -4,3 +4,29 @@ const repoCount = 50
 
 // String Interpolation
 console.log(`Hello my name is ${name} and mu repo count is ${repoCount}`) 
+
+const gameName = new String('aakash-sh')
+
+console.log(gameName[0])
+console.log(gameName.__proto__)
+
+console.table([gameName.length,gameName.toUpperCase, gameName.charAt(2), gameName.indexOf('a')])
+
+const newString = gameName.substring(0,3)
+console.log(newString)
+
+const anotherString = gameName.slice(-8,4)
+
+console.log(anotherString)
+
+const newString1 = "  aakash      "
+
+console.log(newString1)
+console.log(newString1.trim())
+
+const url = "https://aakutu.com"
+
+console.log(url.replace('akutu','ktu'))
+console.log(url.includes('tu'))
+
+console.log(gameName.split('-'))
