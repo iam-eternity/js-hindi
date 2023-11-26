daa52c2dc95990159885ec65b70d0f9451462eb8
eternity <114994799+iam-eternity@users.noreply.github.com>
2023-11-26 07:51:16 +0000

    strings in js

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,6 @@
+const name = "Aakash"
+const repoCount = 50
+// console.log(name + repoCount + " Value") bad practice
+
+// String Interpolation
+console.log(`Hello my name is ${name} and mu repo count is ${repoCount}`) 
