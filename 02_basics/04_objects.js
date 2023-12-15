0dc846ba6264604b5d05d1c548a5fe439fa83916
eternity <114994799+iam-eternity@users.noreply.github.com>
2023-12-15 09:53:24 +0000

    objects in js

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,47 @@
+const tinderUser = new Object()
+
+
+tinderUser.id = "123a"
+tinderUser.name = "Beyonder"
+tinderUser.isLoggedIn = false
+console.log(tinderUser)
+const regularUser = {
+    email: "aaks@123",
+    full_Name : {
+        user_FullName:  {
+            firstname :"Lucifer",
+            lastName: "MorningStar"
+        }
+    }
+}
+console.log(regularUser.full_Name.user_FullName.firstname)
+
+const obj1 = {1: "A", 2: "B"}
+const obj2 = {1: "a", 2: "b"}
+// const obj3 = {obj1, obj2}
+// assign: Copy the values of all of the enumerable own properties from one or more source objects to a target object.
+// const obj3 = Object.assign({}, obj1, obj2)
+const obj3 = {...obj1,...obj2}
+
+// console.log(obj3)
+const users = [
+    {
+        id: 1,
+        email: "aakaa@123"
+    },
+    {
+        id: 2,
+        email: "aakaas@123"
+    },
+    {
+        id: 3,
+        email: "aakaash@123"
+    }
+]
+
+console.log(users[1].email)
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
