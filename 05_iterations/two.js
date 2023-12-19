fe12eae7fca6de7bec1c89b9e42756c1f23fa3a6
eternity <114994799+iam-eternity@users.noreply.github.com>
2023-12-19 19:05:18 +0000

    while do while llop

---
+++ 05_iterations/two.js
@@ -0,0 +1,16 @@
+let index = 0
+while (index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index++;
+}
+let myArray = [ "FLASH","SUPERMAN","BATMAN"]
+let arr = 0;
+while (arr < myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr++;
+}
+let score = 1
+do {
+    console.log(`Score is ${score}`);
+    score += 2
+} while (score < 10)
