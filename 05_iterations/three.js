dcfd88a9aed7ff5810fb95ab0f6f66bfc45d3046
eternity <114994799+iam-eternity@users.noreply.github.com>
2023-12-20 07:19:50 +0000

    for of loop

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -1,1 +1,31 @@
-// for of loop
+// for of  (Array specific loop)
+
+const arr = [1,2,3,4,5]
+
+for (const num of arr) {
+    console.log(num);
+}
+
+const greeting = "Hello World"
+for (const greet of greeting) {
+    console.log(`Each char is ${greet}`);
+}
+
+// Maps
+const map = new Map()
+map.set('IN',"India")
+map.set("US","United States of America")
+map.set("Fr","France")
+console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key, " -> ", value);
+}
+
+const myObject = {
+    "game1" : "NFS",
+    "game2" : "God of war"
+}
+// for (const key of myObject) {
+//     console.log(key);
+// }
