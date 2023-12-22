c8d95843e0e7da62b46ea18ec5d837a8bfd06325
eternity <114994799+iam-eternity@users.noreply.github.com>
2023-12-22 11:37:18 +0000

    Dom in js

---
+++ 06_DOM/one.js
@@ -0,0 +1,11 @@
+console.log(document.getElementById("title"))
+/*
+title.textContent
+'DOM learning on chai aur code  TEXT TEXT '
+title.textContent
+'DOM learning on chai aur code  TEXT TEXT '
+title.innerText
+'DOM learning on chai aur code'
+title.innerHTML
+'DOM learning on chai aur code <span style="display: none;"> TEXT TEXT</span> '
+*/
