454e01e6fa0e68bd2a67573d0d282c86657abeb3
eternity <114994799+iam-eternity@users.noreply.github.com>
2024-01-26 19:33:52 +0000

    pr

--- 09_advance_one/promise.js
+++ 09_advance_one/promise.js
@@ -62,9 +62,45 @@
 
 const promise5 = new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve({username: 'Beyonder', powers: 'Reality warping \n Nigh-omnipotence', origin: 'Beyond Realm', firstApperarence: 'Secret Wars 1'})
+        let error = true
+        if (!error) {
+            resolve({username: 'Beyonder', powers: 'Reality warping \nNigh-omnipotence', origin: 'Beyond Realm', firstApperarence: 'Secret Wars 1'})
+        } else {
+            reject('ERROR: Something went wrong')
+        }
     },1000)
 })
 async function consumePromise5() {
-    console.log(resolve);
+    try {
+        const response = await promise5
+        console.log(response);
+        // Return the resolved value
+        return response // if we do not return response then the consumePromise5 function returns a promise itself, and when you use consumePromise5().then(...), you are trying to handle the result of the outer promise, not the result of the await promise5 within the consumePromise5 function.
+    } catch (error) {
+        console.log(error);
+    }
 }
+consumePromise5()
+// .then((res) => {
+//     console.log(res.origin);
+// })
+
+async function getAllUsers() {
+    try {
+        const response = await fetch('https://catfact.ninja/fact')
+        const data = await response.json(); // Parse response as JSON
+        console.log(data.fact);
+    } 
+    catch(error) {
+        console.log('Error : ', error);
+    }
+}
+// getAllUsers()
+fetch('https://official-joke-api.appspot.com/random_joke')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(`${data.setup} \n${data.punchline}`);
+})
+.catch((error) => console.log(error))
