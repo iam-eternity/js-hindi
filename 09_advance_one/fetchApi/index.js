f80f861f72b14620cf2731165f9d425b00fc3838
eternity <114994799+iam-eternity@users.noreply.github.com>
2024-01-30 13:46:47 +0000

    pokemon api

--- 09_advance_one/fetchApi/index.js
+++ 09_advance_one/fetchApi/index.js
@@ -4,6 +4,30 @@
 //         used for interacting with APIs to retrieve and send
 //         data asynchronously over the web.
 //         fetch(url, {options})
-fetch('https://pokeapi.co/api/v2/pokemon/mew')
-.then((response) => console.log(response))
-.catch(error => console.log(error))
+// fetch('https://pokeapi.co/api/v2/pokemon/charizard')
+// .then((response) => {
+//     if (!response.ok) {
+//         throw new Error('Could not fetch resource')
+//     } 
+//     return response.json()
+// })
+// .then(data => console.log(data.weight))
+// .catch(error => console.log(error))
+async function fetchData() {
+    try {
+        const pokemonName = document.getElementById('pokemonName').value.toLocaleLowerCase()
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+        if (!response.ok) {
+            throw new Error('Could not fetch resource')
+        } else {
+            const data = await response.json()
+            const pokemonSprite = data.sprites.front_default
+            const imgElement = document.getElementById('pokemonSprite')
+            imgElement.src = pokemonSprite
+            imgElement.style.display = 'block'
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+fetchData()
